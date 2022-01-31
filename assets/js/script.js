bad6b8c0dc51d542aa55904f793f10b0ad252e72
cedean1029 <cedean1029@gmail.com>
2022-01-30 21:31:20 -0500

    edits to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,5 @@
 var hours = {}
 
-// $(".textarea").on("click", "p", function () {
-//     var textInput = $("<textarea>").addClass("description").val(text);
-//     $(this).replaceWith(textInput);
-//     console.log(textInput)
-// })
-
-// $(".fa-save").on("click", function (event) {
-//     console.log($(event.target).parent()[0].previousElementSibling.value);
-//     console.log($(event.target).parent()[0].previousElementSibling.id);
-
-//     var text = $(event.target).parent()[0].previousElementSibling.value;
-//     var id = $(event.target).parent()[0].previousElementSibling.id;
-
-//     localStorage.setItem(id, text);
-// });
-
 function save(e) {
     console.log(e.target.parentElement.previousElementSibling.value);
     console.log(e.target.parentElement.previousElementSibling.id);
@@ -30,26 +14,25 @@ function loadHours() {
     var messages = Object.entries(localStorage);
     messages.forEach(function (message) {
         console.log(message)
-        $(`#${message[0]}`).val(message[1])
+        document.getElementById(message[0]).innerHTML=message[1]
     })
 };
 
 function auditHours() {
     // get hour from task element
-    var timeName = $(".hour").text();
     var currentTime = moment().format("H");
-    console.log(timeName);
     console.log(currentTime);
 
     for (var i = 9; i <= 17; i++) {
-        if (parseInt($("#"+i)[0].id) < currentTime) {
-            $("#"+i).addClass("past")
+        var id = document.getElementById(i)
+        if (i < currentTime) {
+            id.setAttribute("class", "past") 
         }
-        else if ($("#"+i)[0].id == currentTime) {
-            $("#"+i).addClass("present")
+        else if (i == currentTime) {
+            id.setAttribute("class", "present") 
         }
-        else if ($("#"+i)[0].id > currentTime) {
-            $("#"+i).addClass("future")
+        else if (i > currentTime) {
+            id.setAttribute("class", "future") 
         };
     };
 };
